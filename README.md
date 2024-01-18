7a692f7469b68f9a57fb77b01aa57e0380d1b9d9
antitux <antitux@gmail.com>
2024-01-18 15:01:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Countdown Timer
+
+Made from scratch for running timers for things, such as Ads on Twitch.
+
+## VCR Font
+
+No idea where this originally came from. If it ends up having a license, I'll replace it with something else.
+
+## Usage
+
+Deploy to a web server or run locally. It's static html. You could probably very easily turn both of these into a streamelements extension pretty easily.
+
+### Variables
+| VAR  |  DEFAULT VALUE |
+|---|---|
+| message | Ads Running: |
+| timer  |  15 (seconds) |
+
+You can also host it locally using a url like this, which will give you a 120 second timer with a message that says PONY:
+
+```file://path/to/countdown/index.html?message=pony&timer=120```
+
+
+I've provided a manifest.yml to push to CloudFoundry, as that's what I use to host most of my projects. This could easily be dockerized too, but I'm lazy.
+
+Any other questions, feel free to [Shoot Me An Email](mailto:github@antitux.dev)
---
+++ assets/countdown.js
@@ -0,0 +1,47 @@
+// Get all values
+const searchParams = new URLSearchParams(window.location.search);
+
+
+// get "timer" value. Default to 15 seconds.
+if (searchParams.has('timer')) {
+    console.log(searchParams.get('timer'));
+    timeLeft = searchParams.get('timer');
+} else {
+    timeLeft = "15";
+}
+
+// Get "message" value. Default to "Ads Running:"
+if (searchParams.has('message')) {
+    console.log(searchParams.get('message'));
+    message = searchParams.get('message');
+} else {
+    message = "Ads Running:";
+}
+
+// Run the countdown
+function countdown() {
+    // Subtract from time left
+    timeLeft--;
+    // Set the initial message how you want it to look.
+    document.getElementById("message").innerHTML = String( message ) + "<br /> " + String(timeLeft);
+    
+    // If the timer still has time left, run it. Otherwise, hide it.
+    if (timeLeft > 0) {
+        setTimeout(countdown, 1000);
+    } else {
+        document.getElementById("message").innerHTML = "" ;
+    }
+    
+    // If the timer is less than 11 seconds, set to #00FFFF
+    if (timeLeft < 11) {     
+        document.getElementById('message').style.color = '#00FFFF';
+    }
+
+    
+
+
+};
+
+// Run the whole goddamn thing.
+setTimeout(countdown, 1000);
+
